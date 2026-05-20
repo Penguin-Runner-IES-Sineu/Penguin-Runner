97ea487453ad910a14d54c3c21840a64469f4ff9
Marc Mas <marcmashernandez@gmail.com>
2026-05-20 18:32:36 +0200

    Creat projecte

---
+++ LodeRunner/src/main/java/org/iessineu/loderunner/LodeRunner.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.iessineu.loderunner;
+
+/**
+ *
+ * @author Marc Mas
+ */
+public class LodeRunner {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
