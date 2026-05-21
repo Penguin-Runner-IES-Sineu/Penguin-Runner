e1cdfe4e5f743fb27842fece95db5990c9a19456
Marc Mas <marcmashernandez@gmail.com>
2026-05-21 19:52:31 +0200

    test

--- JuegoCasillas/JuegoCasillas/src/main/java/com/mycompany/juegocasillas/JuegoCasillas.java
+++ JuegoCasillas/JuegoCasillas/src/main/java/com/mycompany/juegocasillas/JuegoCasillas.java
@@ -7,14 +7,24 @@ package com.mycompany.juegocasillas;
  *
  * @author loren
  */
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class JuegoCasillas extends JPanel implements KeyListener {
 
     static final int TILE = 48;
-
+    
     String[] mapa = {
         "##########",
         "#.........#",
