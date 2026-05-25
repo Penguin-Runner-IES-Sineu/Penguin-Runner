d0fc3391e0155257e8f84b9c97f2a091e6cd6e4a
Marc Mas <marcmashernandez@gmail.com>
2026-05-25 19:52:19 +0200

    json (hoy no, mañana)

--- PenguinRunner/src/main/java/iessineu/penguinrunner/Mapa/Mapes.java
+++ PenguinRunner/src/main/java/iessineu/penguinrunner/Mapa/Mapes.java
@@ -2,15 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package iessineu.penguinrunner;
+package iessineu.penguinrunner.mapa;
 
 /**
  *
  * @author loren
  */
-public class CrearMapa {
+public class Mapes {
 
-    String mapa[] = {
+    String mapes[] = {
         "############################",
         "#..............................#",
         "#.............G................#",
