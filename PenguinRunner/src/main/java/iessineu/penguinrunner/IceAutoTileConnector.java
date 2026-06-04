fefda2c8fe9ee2cf6ed72b94fcdb66e78c56bb5f
LlorencDAM <llorenc7280@iessineu.net>
2026-06-04 14:04:44 +0200

    asd

--- PenguinRunner/src/main/java/iessineu/penguinrunner/IceAutoTileConnector.java
+++ PenguinRunner/src/main/java/iessineu/penguinrunner/IceAutoTileConnector.java
@@ -8,19 +8,32 @@ package iessineu.penguinrunner;
  *
  * @author loren
  */
-
 import iessineu.penguinrunner.Blocks.Block;
 import iessineu.penguinrunner.Blocks.TileType;
 
 public class IceAutoTileConnector {
 
+    private static final int N = 1;
+    private static final int E = 2;
+    private static final int S = 4;
+    private static final int W = 8;
+
+    private static final int NE = 16;
+    private static final int SE = 32;
+    private static final int SW = 64;
+    private static final int NW = 128;
+
     public static void update(Block[][] blocks) {
-        if (blocks == null || blocks.length == 0 || blocks[0].length == 0) {
+        if (blocks == null || blocks.length == 0) {
             return;
         }
 
         for (int row = 0; row < blocks.length; row++) {
-            for (int col = 0; col < blocks[0].length; col++) {
+            if (blocks[row] == null) {
+                continue;
+            }
+
+            for (int col = 0; col < blocks[row].length; col++) {
                 Block block = blocks[row][col];
 
                 if (!isIce(block)) {
@@ -28,6 +41,7 @@ public class IceAutoTileConnector {
                 }
 
                 String printableKey = getIcePrintableKey(blocks, row, col);
+
                 block.setPrintableKey(printableKey);
                 block.setPrintables();
             }
@@ -35,47 +49,216 @@ public class IceAutoTileConnector {
     }
 
     private static String getIcePrintableKey(Block[][] blocks, int row, int col) {
+        int mask = getMask(blocks, row, col);
+        String logicalKey = getLogicalIceKey(mask);
+
+        return getRealIceAssetName(logicalKey);
+    }
+
+    private static int getMask(Block[][] blocks, int row, int col) {
         boolean n = hasIce(blocks, row - 1, col);
         boolean e = hasIce(blocks, row, col + 1);
         boolean s = hasIce(blocks, row + 1, col);
         boolean w = hasIce(blocks, row, col - 1);
 
+        boolean ne = hasIce(blocks, row - 1, col + 1);
+        boolean se = hasIce(blocks, row + 1, col + 1);
+        boolean sw = hasIce(blocks, row + 1, col - 1);
+        boolean nw = hasIce(blocks, row - 1, col - 1);
+
         int mask = 0;
 
-        if (n) mask |= 1;
-        if (e) mask |= 2;
-        if (s) mask |= 4;
-        if (w) mask |= 8;
+        if (n) {
+            mask |= N;
+        }
+        if (e) {
+            mask |= E;
+        }
+        if (s) {
+            mask |= S;
+        }
+        if (w) {
+            mask |= W;
+        }
 
-        return getBaseIceKey(mask);
+        /*
+         * Les diagonals només compten si també existeixen
+         * els dos costats que formen aquella cantonada.
+         *
+         * Per exemple:
+         * NE només importa si hi ha N i E.
+         */
+        if (n && e && ne) {
+            mask |= NE;
+        }
+        if (e && s && se) {
+            mask |= SE;
+        }
+        if (s && w && sw) {
+            mask |= SW;
+        }
+        if (w && n && nw) {
+            mask |= NW;
+        }
+
+        return mask;
     }
 
-    private static String getBaseIceKey(int mask) {
-        return switch (mask) {
-            case 0 -> "ice_isolated";
+    private static String getLogicalIceKey(int mask) {
+        boolean n = (mask & N) != 0;
+        boolean e = (mask & E) != 0;
+        boolean s = (mask & S) != 0;
+        boolean w = (mask & W) != 0;
 
-            case 1 -> "ice_n";
-            case 2 -> "ice_e";
-            case 4 -> "ice_s";
-            case 8 -> "ice_w";
+        boolean ne = (mask & NE) != 0;
+        boolean se = (mask & SE) != 0;
+        boolean sw = (mask & SW) != 0;
+        boolean nw = (mask & NW) != 0;
 
-            case 5 -> "ice_ns";
-            case 10 -> "ice_ew";
+        int cardinalMask = 0;
 
-            case 3 -> "ice_ne";
-            case 6 -> "ice_es";
-            case 12 -> "ice_sw";
-            case 9 -> "ice_wn";
+        if (n) {
+            cardinalMask |= N;
+        }
+        if (e) {
+            cardinalMask |= E;
+        }
+        if (s) {
+            cardinalMask |= S;
+        }
+        if (w) {
+            cardinalMask |= W;
+        }
 
-            case 7 -> "ice_nes";
-            case 14 -> "ice_esw";
-            case 13 -> "ice_swn";
-            case 11 -> "ice_wne";
+        String key = getBaseIceKey(cardinalMask);
 
-            case 15 -> "ice_center";
+        switch (cardinalMask) {
+            case N | E -> {
+                if (!ne) {
+                    key += "_in_ne";
+                }
+            }
 
-            default -> "ice_center";
+            case E | S -> {
+                if (!se) {
+                    key += "_in_se";
+                }
+            }
+
+            case S | W -> {
+                if (!sw) {
+                    key += "_in_sw";
+                }
+            }
+
+            case W | N -> {
+                if (!nw) {
+                    key += "_in_nw";
+                }
+            }
+
+            case N | E | S -> {
+                if (!ne) {
+                    key += "_in_ne";
+                }
+                if (!se) {
+                    key += "_in_se";
+                }
+            }
+
+            case E | S | W -> {
+                if (!se) {
+                    key += "_in_se";
+                }
+                if (!sw) {
+                    key += "_in_sw";
+                }
+            }
+
+            case S | W | N -> {
+                if (!sw) {
+                    key += "_in_sw";
+                }
+                if (!nw) {
+                    key += "_in_nw";
+                }
+            }
+
+            case W | N | E -> {
+                if (!nw) {
+                    key += "_in_nw";
+                }
+                if (!ne) {
+                    key += "_in_ne";
+                }
+            }
+
+            case N | E | S | W -> {
+                if (!ne) {
+                    key += "_in_ne";
+                }
+                if (!se) {
+                    key += "_in_se";
+                }
+                if (!sw) {
+                    key += "_in_sw";
+                }
+                if (!nw) {
+                    key += "_in_nw";
+                }
+            }
+        }
+
+        return key;
+    }
+
+    private static String getBaseIceKey(int cardinalMask) {
+        return switch (cardinalMask) {
+            case 0 ->
+                "ice_isolated";
+
+            case N ->
+                "ice_n";
+            case E ->
+                "ice_e";
+            case S ->
+                "ice_s";
+            case W ->
+                "ice_w";
+
+            case N | S ->
+                "ice_ns";
+            case E | W ->
+                "ice_ew";
+
+            case N | E ->
+                "ice_ne";
+            case E | S ->
+                "ice_es";
+            case S | W ->
+                "ice_sw";
+            case W | N ->
+                "ice_wn";
+
+            case N | E | S ->
+                "ice_nes";
+            case E | S | W ->
+                "ice_esw";
+            case S | W | N ->
+                "ice_swn";
+            case W | N | E ->
+                "ice_wne";
+
+            case N | E | S | W ->
+                "ice_center";
+
+            default ->
+                "ice_center";
         };
+    }
+
+    private static String getRealIceAssetName(String logicalKey) {
+        return logicalKey;
     }
 
     private static boolean hasIce(Block[][] blocks, int row, int col) {
@@ -83,7 +266,11 @@ public class IceAutoTileConnector {
             return false;
         }
 
-        if (col < 0 || col >= blocks[0].length) {
+        if (blocks[row] == null) {
+            return false;
+        }
+
+        if (col < 0 || col >= blocks[row].length) {
             return false;
         }
 
