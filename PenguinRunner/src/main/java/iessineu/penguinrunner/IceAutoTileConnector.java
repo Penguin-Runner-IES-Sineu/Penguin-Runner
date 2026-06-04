b442e1f20b7c32e5e1717cfa777b74c4079fa763
LlorencDAM <llorenc7280@iessineu.net>
2026-06-04 04:32:18 +0200

    asd

--- PenguinRunner/src/main/java/iessineu/penguinrunner/IceAutoTileConnector.java
+++ PenguinRunner/src/main/java/iessineu/penguinrunner/IceAutoTileConnector.java
@@ -9,7 +9,6 @@ package iessineu.penguinrunner;
  * @author loren
  */
 
-
 import iessineu.penguinrunner.Blocks.Block;
 import iessineu.penguinrunner.Blocks.TileType;
 
@@ -29,7 +28,6 @@ public class IceAutoTileConnector {
                 }
 
                 String printableKey = getIcePrintableKey(blocks, row, col);
-
                 block.setPrintableKey(printableKey);
                 block.setPrintables();
             }
@@ -44,30 +42,10 @@ public class IceAutoTileConnector {
 
         int mask = 0;
 
-        if (n) {
-            mask |= 1;
-        }
-
-        if (e) {
-            mask |= 2;
-        }
-
-        if (s) {
-            mask |= 4;
-        }
-
-        if (w) {
-            mask |= 8;
-        }
-
-        if (mask == 15) {
-            boolean ne = hasIce(blocks, row - 1, col + 1);
-            boolean se = hasIce(blocks, row + 1, col + 1);
-            boolean sw = hasIce(blocks, row + 1, col - 1);
-            boolean nw = hasIce(blocks, row - 1, col - 1);
-
-            return getCenterWithInnerCorners(ne, se, sw, nw);
-        }
+        if (n) mask |= 1;
+        if (e) mask |= 2;
+        if (s) mask |= 4;
+        if (w) mask |= 8;
 
         return getBaseIceKey(mask);
     }
@@ -96,62 +74,6 @@ public class IceAutoTileConnector {
 
             case 15 -> "ice_center";
 
-            default -> "ice";
-        };
-    }
-
-    private static String getCenterWithInnerCorners(
-            boolean ne,
-            boolean se,
-            boolean sw,
-            boolean nw
-    ) {
-        boolean innerNE = !ne;
-        boolean innerSE = !se;
-        boolean innerSW = !sw;
-        boolean innerNW = !nw;
-
-        int innerMask = 0;
-
-        if (innerNE) {
-            innerMask |= 1;
-        }
-
-        if (innerSE) {
-            innerMask |= 2;
-        }
-
-        if (innerSW) {
-            innerMask |= 4;
-        }
-
-        if (innerNW) {
-            innerMask |= 8;
-        }
-
-        return switch (innerMask) {
-            case 0 -> "ice_center";
-
-            case 1 -> "ice_inner_ne";
-            case 2 -> "ice_inner_se";
-            case 4 -> "ice_inner_sw";
-            case 8 -> "ice_inner_nw";
-
-            case 3 -> "ice_inner_ne_se";
-            case 6 -> "ice_inner_se_sw";
-            case 12 -> "ice_inner_sw_nw";
-            case 9 -> "ice_inner_nw_ne";
-
-            case 5 -> "ice_inner_ne_sw";
-            case 10 -> "ice_inner_nw_se";
-
-            case 7 -> "ice_inner_ne_se_sw";
-            case 14 -> "ice_inner_se_sw_nw";
-            case 13 -> "ice_inner_sw_nw_ne";
-            case 11 -> "ice_inner_nw_ne_se";
-
-            case 15 -> "ice_inner_all";
-
             default -> "ice_center";
         };
     }
@@ -169,10 +91,6 @@ public class IceAutoTileConnector {
     }
 
     private static boolean isIce(Block block) {
-        if (block == null) {
-            return false;
-        }
-
-        return block.getType() == TileType.ICE;
+        return block != null && block.getType() == TileType.ICE;
     }
 }
